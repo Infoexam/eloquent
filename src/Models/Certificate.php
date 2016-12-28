2a5f1c6b31d4eca8317025c2cf8e6ead3106c47d
BePsvPT <freedom@csie.io>
2016-12-28 14:23:15 +0800

    Hide password field and add relation to certificate

--- src/Models/Certificate.php
+++ src/Models/Certificate.php
@@ -4,5 +4,13 @@ namespace Infoexam\Eloquent\Models;
 
 class Certificate extends Model
 {
-    //
+    /**
+     * Get the category that belongs to the certificate.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- src/Models/User.php
+++ src/Models/User.php
@@ -17,7 +17,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      *
      * @var array
      */
-    protected $hidden = [];
+    protected $hidden = ['password'];
 
     /**
      * The attributes that should be mutated to dates.
